e606b27c9b2ddf72d1c3d907423e8b80e6cb243c
Aijkl <51302983+aijkl@users.noreply.github.com>
2023-05-01 22:00:52 +0900

    CRLF以外を検出したい

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,25 @@
 
 FOLDER=$1
 EXCLUDE=$2
-echo "searching for CRLF endings in: $FOLDER"
+echo "searching for non-CRLF line endings in: $FOLDER"
 
 FILES_TYPES="$(find "$FOLDER" ! -path "./.git/*" -not -type d -exec file "{}" ";")"
-FILES_WITH_CRLF=$(echo "$FILES_TYPES" | grep " CRLF " | cut -d " " -f 1 | cut -d ":" -f 1)
+FILES_WITHOUT_CRLF=$(echo "$FILES_TYPES" | grep -v " CRLF " | cut -d " " -f 1 | cut -d ":" -f 1)
 
 for word in $EXCLUDE; do
-  FILES_WITH_CRLF=$(echo "$FILES_WITH_CRLF" | grep --invert-match -E "$word")
+  FILES_WITHOUT_CRLF=$(echo "$FILES_WITHOUT_CRLF" | grep --invert-match -E "$word")
 done
 
 BOLD_RED='\033[1;31m'
 BOLD_GREEN='\033[1;32m'
 NO_COLOR='\033[0m'
 
-if [ -z "$FILES_WITH_CRLF" ]; then
-  echo -e "${BOLD_GREEN}No files with CRLF endings found.${NO_COLOR}"
+if [ -z "$FILES_WITHOUT_CRLF" ]; then
+  echo -e "${BOLD_GREEN}No files with non-CRLF line endings found.${NO_COLOR}"
   exit 0
 else
-  NR_FILES=$(echo "$FILES_WITH_CRLF" | wc -l)
-  echo -e "${BOLD_RED}Found ${NR_FILES} files with CRLF endings.${NO_COLOR}"
-  echo "$FILES_WITH_CRLF"
+  NR_FILES=$(echo "$FILES_WITHOUT_CRLF" | wc -l)
+  echo -e "${BOLD_RED}Found ${NR_FILES} files with non-CRLF line endings.${NO_COLOR}"
+  echo "$FILES_WITHOUT_CRLF"
   exit "$NR_FILES"
 fi
